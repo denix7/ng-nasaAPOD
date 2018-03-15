7009f86dd3a4392f859ea0f47e5024dc6114fcb1
Dennis <dennis.brian.pv@gmail.com>
2018-03-15 19:46:12 -0400

    Creacion de toolbar button and Icon

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  MatToolbarModule, 
+  MatToolbar,
+  MatButtonModule,
+  MatButton,
+  MatIconModule
+} from '@angular/material';
+
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [MaterialDemoComponent]
 })
