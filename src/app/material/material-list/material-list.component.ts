c67ce82e25b0a325542cfaa6fd74325719d04264
Dennis <dennis.brian.pv@gmail.com>
2018-03-20 22:02:04 -0400

    Agregando router link al menu

--- src/app/material/material-list/material-list.component.ts
+++ src/app/material/material-list/material-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Estudiante } from '../../shared/model/estudiante';
+import { ClassEstudiante } from '../../shared/model/class-estudiante';
 
 @Component({
   selector: 'ed-material-list',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./material-list.component.css']
 })
 export class MaterialListComponent implements OnInit {
-
+  estudiante1: ClassEstudiante;
+  estudiante2: Estudiante;
+  estudiante3: Estudiante;
   constructor() { }
 
   ngOnInit() {
+    console.log('Inicializando el componente MaterialList');
+    this.estudiante1 = new ClassEstudiante(1, 'Dennis Padilla', 'Cochabamba');
+    this.estudiante2 = {
+      id: 2,
+      nombre: 'Elon Musk',
+      ciudad: 'LA'
+    };
+    this.estudiante3 = {
+      id: 3,
+      nombre: 'Bill Gates',
+      ciudad: 'NY'
+    };
+
+    setTimeout(()=>{
+      this.estudiante3 = {
+        id: 4,
+        nombre: 'Juan lopez',
+        ciudad: 'Santiago'
+      }
+    },3000);
   }
 
 }
