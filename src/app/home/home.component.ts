1aade94a87f112d4a739f624fb7c99dcdbaf0920
Dennis <dennis.brian.pv@gmail.com>
2018-03-22 12:33:47 -0400

    Implementando inyector de dependencias y reando servicio nasa-api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Apod } from '../shared/model/apod';
+import { NasaApiService } from '../shared/services/nasa-api.service';
 
 @Component({
   selector: 'ed-home',
@@ -9,18 +10,11 @@ import { Apod } from '../shared/model/apod';
 export class HomeComponent implements OnInit {
 
   apod: Apod;
-  constructor() { }
+  constructor(private nasaApi: NasaApiService) {
+   }
 
   ngOnInit() {
-    this.apod = {
-      "date": "2017-07-09", 
-      "explanation": "Can you find your favorite country or city?  Surprisingly, on this world-wide nightscape, city lights make this task quite possible.  Human-made lights highlight particularly developed or populated areas of the Earth's surface, including the seaboards of Europe, the eastern United States, and Japan.  Many large cities are located near rivers or oceans so that they can exchange goods cheaply by boat.  Particularly dark areas include the central parts of South America, Africa, Asia, and Australia.  The featured composite was created from images that were collected during cloud-free periods in April and October 2012 by the Suomi-NPP satellite, from a polar orbit about 824 kilometers above the surface, using its Visible Infrared Imaging Radiometer Suite (VIIRS).", 
-      "hdurl": "https://apod.nasa.gov/apod/image/1707/EarthAtNight_SuomiNPP_3600.jpg", 
-      "media_type": "image", 
-      "service_version": "v1", 
-      "title": "Earth at Night", 
-      "url": "https://apod.nasa.gov/apod/image/1707/EarthAtNight_SuomiNPP_1080.jpg"
-    }
+    this.apod = this.nasaApi.getApod();
   }
 
 }
