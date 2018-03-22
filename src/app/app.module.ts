f2c9a2222c1532dea332ec6da5de7a0ddc010da0
Dennis <dennis.brian.pv@gmail.com>
2018-03-22 17:53:54 -0400

    Usando servicio de api de la nasa con httpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import {HttpClientModule} from '@angular/common/http';
+//import {HttpModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { NasaApiService } from './shared/services/nasa-api.service';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    HttpClientModule
   ],
   providers: [NasaApiService],
   bootstrap: [AppComponent]
--- src/app/shared/services/nasa-api.service.ts
+++ src/app/shared/services/nasa-api.service.ts
@@ -1,22 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Apod } from '../model/apod';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 
+const APOD_URL = 'https://api.nasa.gov/planetary/apod';
+const API_KEY = 'eVGwuW02is2fDlnoNyyCB0cz08ySIpjdtmVNvv8W'
 @Injectable()
 export class NasaApiService {
+  //Dependency Injection: HttpClient
+  constructor(private httpClient: HttpClient) { }
 
-  private readonly DATA: Apod = {
-    "date": "2017-07-09", 
-    "explanation": "Can you find your favorite country or city?  Surprisingly, on this world-wide nightscape, city lights make this task quite possible.  Human-made lights highlight particularly developed or populated areas of the Earth's surface, including the seaboards of Europe, the eastern United States, and Japan.  Many large cities are located near rivers or oceans so that they can exchange goods cheaply by boat.  Particularly dark areas include the central parts of South America, Africa, Asia, and Australia.  The featured composite was created from images that were collected during cloud-free periods in April and October 2012 by the Suomi-NPP satellite, from a polar orbit about 824 kilometers above the surface, using its Visible Infrared Imaging Radiometer Suite (VIIRS).", 
-    "hdurl": "https://apod.nasa.gov/apod/image/1707/EarthAtNight_SuomiNPP_3600.jpg", 
-    "media_type": "image", 
-    "service_version": "v1", 
-    "title": "Earth at Night", 
-    "url": "https://apod.nasa.gov/apod/image/1707/EarthAtNight_SuomiNPP_1080.jpg"
-  };
-
-  constructor() { }
-
-  getApod(): Apod{
-    return this.DATA;
+  getApod(): Observable<Apod>{
+    //return this.DATA;
+    return this.httpClient.get<Apod>(APOD_URL + '?api_key=' + API_KEY);
   }
 }
